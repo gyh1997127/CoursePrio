e0eb3adbc9af9075a99866c8b992ce95fef49976
yuhuig <yuhuig@ensc-mmc-03.engineering.sfu.ca>
2021-06-17 15:07:29 -0700

    systolic

--- src/mm.cpp
+++ src/mm.cpp
@@ -1,25 +1,43 @@
 #include "mm.h"
-
 void kernel_gemm(float C[NI * NJ], float A[NI * NK], float B[NK * NJ],
                  float alpha, float beta) {
+  #pragma HLS ARRAY_PARTITION variable=C dim=1 cyclic factor=64
+  #pragma HLS ARRAY_PARTITION variable=A dim=1 cyclic factor=64
+  #pragma HLS ARRAY_PARTITION variable=B dim=1 cyclic factor=64
   int i, j, k;
+  float temp_sum[NJ];
 
   // => Form C := alpha*A*B + beta*C,
   // A is NIxNK
   // B is NKxNJ
   // C is NIxNJ
+  
+  //MM_i:for (i = 0; i < NI; i++) {
+  //  MM_k:for (k = 0; k < NK; ++k) {
+  //    C[i * NJ + k] *= beta;
+  //    MM_j:for (j = 0; j < NJ; j++) {
+  //      #pragma HLS PIPELINE II=1
+  //      int temp = (k==0) ? 0 : temp_sum[j];
+  //      temp += alpha * A[i * NK + k] * B[k * NJ + j];
+  //      temp_sum[j] = temp;
+  //      if (k == NJ - 1) C[i * NJ + j] = temp;
+  //      //C[i * NJ + j] += alpha * A[i * NK + k] * B[k * NJ + j];
+  //    }
+  //  }
+  //}
 
-  InitC_i:for (i = 0; i < NI; i++) {
-    InitC_j:for (j = 0; j < NJ; j++) {
-      C[i * NJ + j] *= beta;
-    }
-
-  MM_j:for (j = 0; j < NJ; j++) {
-    MM_k:for (k = 0; k < NK; ++k) {
-        C[i * NJ + j] += alpha * A[i * NK + k] * B[k * NJ + j];
-      }
-    }
-  }
+  K:for(k=0; k<NK; k++) {
+      I:for (i=0; i<NI; i++) {
+          J: for (j=0; j<NJ; j++) {
+	       int last = (k==0) ? C[i * NJ + j]*beta : C[i * NJ + j];
+	       //update new sum
+	       int a_val = (i<NI && k<NK) ? A[i * NK + k] : 0;
+	       int b_val = (k<NK && j<NJ) ? B[k * NJ + j] : 0;
+	       int temp = alpha * a_val * b_val;
+	       //update C
+	       C[i * NJ + j] = temp;
+	  }
+      } 
+     }
 }
-
 
--- src/mm.h
+++ src/mm.h
@@ -6,6 +6,7 @@
 #define NJ 2048
 #define NK 2048
 #define BSize 32
+#define FACTOR NI/BSize
 
 void kernel_gemm(float C[NI * NJ], float A[NI * NK], float B[NK * NJ], float alpha, float beta);
 
