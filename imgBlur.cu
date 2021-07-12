793edb69f18dca2b3effbba8a0f92dba4ca381bc
Yuhui Gao <yuhuig@gmail.com>
2021-07-12 16:31:23 -0700

    added boundary check

--- imgBlur.cu
+++ imgBlur.cu
@@ -11,85 +11,85 @@
   } while (0)
 
 #define BLUR_SIZE 21
-#define nthreads 64
+#define nthreads 16
 
 ///////////////////////////////////////////////////////
 __device__ void blur_x(float *out, float *in, int width, int height) {
-  int r = BLUR_SIZE;
-  float scale = 1.0 / (float)((r << 1) + 1); // 1/(2r+1)
+  //int r = BLUR_SIZE;
+  float scale = 1.0 / (float)((BLUR_SIZE << 1) + 1); // 1/(2r+1)
 
   /////////////////////
   // col [0, BLUR_SIZE]
   float t;
-  t = in[0] * r; // padding with in[0]
+  t = in[0] * BLUR_SIZE; // padding with in[0]
 
   // accumulate BLUR_SIZE pixels to in[0]'s right
   // and average out
-  for (int x = 0; x < (r + 1); x++) {
+  for (int x = 0; x < (BLUR_SIZE + 1); x++) {
     t += in[x];
   }
   out[0] = t * scale;
 
   // take care of 0<x<BLUR_SIZE+1
-  for (int x = 1; x < (r + 1); x++) {
-    t += in[x + r];
+  for (int x = 1; x < (BLUR_SIZE + 1); x++) {
+    t += in[x + BLUR_SIZE];
     t -= in[0];  
     out[x] = t * scale;
   }
 
   /////////////////////
   // col [BLUR_SIZE+1, width-BLURSIZE]
-  for (int x = (r + 1); x < width - r; x++) {
-    t += in[x + r];
-    t -= in[x - r - 1];
+  for (int x = (BLUR_SIZE + 1); x < width - BLUR_SIZE; x++) {
+    t += in[x + BLUR_SIZE];
+    t -= in[x - BLUR_SIZE - 1];
     out[x] = t * scale;
   }
 
   /////////////////////
   // col [width-BLURSIZE, width]
-  for (int x = width - r; x < width; x++) {
+  for (int x = width - BLUR_SIZE; x < width; x++) {
     t += in[width - 1]; // padding with in[w-1]
-    t -= in[x - r - 1];
+    t -= in[x - BLUR_SIZE - 1];
     out[x] = t * scale;
   }
 }
 
 __device__ void blur_y(float *out, float *in, int width, int height) {
-  int r = BLUR_SIZE;
-  float scale = 1.0 / (float)((r << 1) + 1); // 1/(2r+1)
+  //int r = BLUR_SIZE;
+  float scale = 1.0 / (float)((BLUR_SIZE << 1) + 1); // 1/(2r+1)
 
   /////////////////////
   // row [0, BLUR_SIZE]
   float t;
-  t = in[0] * r; // padding with in[0]
+  t = in[0] * BLUR_SIZE; // padding with in[0]
 
   // accumulate BLUR_SIZE pixels to in[0]'s top
   // and average out
-  for (int y = 0; y < (r + 1); y++) {
+  for (int y = 0; y < (BLUR_SIZE + 1); y++) {
     t += in[y + width];
   }
   out[0] = t * scale;
 
   // take care of 0<y<BLUR_SIZE+1
-  for (int y = 1; y < (r + 1); y++) {
-    t += in[(y + r) * width];
+  for (int y = 1; y < (BLUR_SIZE + 1); y++) {
+    t += in[(y + BLUR_SIZE) * width];
     t -= in[0];
     out[y * width] = t * scale;
   }
 
   /////////////////////
   // row [BLUR_SIZE+1, width-BLURSIZE]
-  for (int y = (r + 1); y < height - r; y++) {
-    t += in[(y + r) * width];
-    t -= in[(y - r) * width - width];
+  for (int y = (BLUR_SIZE + 1); y < height - BLUR_SIZE; y++) {
+    t += in[(y + BLUR_SIZE) * width];
+    t -= in[(y - BLUR_SIZE) * width - width];
     out[y * width] = t * scale;
   }
 
   /////////////////////
   // row [width-BLURSIZE, width]
-  for (int y = height - r; y < height; y++) {
+  for (int y = height - BLUR_SIZE; y < height; y++) {
     t += in[(height - 1) * width]; // padding with in[h-1]
-    t -= in[(y - r) * width - width];
+    t -= in[(y - BLUR_SIZE) * width - width];
     out[y * width] = t * scale;
   }
 }
@@ -97,12 +97,14 @@ __device__ void blur_y(float *out, float *in, int width, int height) {
 __global__ void blurKernel_x(float *out, float *in, int width, int height) {
   int y = blockIdx.x * blockDim.x + threadIdx.x;
   // accessing by row -> insufficient
-  blur_x(&out[y * width], &in[y * width], width, height);
+  if (y < height)
+    blur_x(&out[y * width], &in[y * width], width, height);
 }
 
 __global__ void blurKernel_y(float *out, float *in, int width, int height) {
   int x = blockIdx.x * blockDim.x + threadIdx.x;
-  blur_y(&out[x], &in[x], width, height);
+  if (x < width)
+    blur_y(&out[x], &in[x], width, height);
 }
 
 ///////////////////////////////////////////////////////
@@ -153,17 +155,24 @@ int main(int argc, char *argv[]) {
              imageWidth * imageHeight * sizeof(float), cudaMemcpyHostToDevice);
 
   // Call your GPU kernel 10 times
-  for (int i = 0; i < 1; i++) {
+  for (int i = 0; i < 10; i++) {
+    printf("Iteration %d\n", i);
     dim3 threadsPerBlock(nthreads);
     // horizontal pass
     {
       dim3 blocksPerGrid(imageHeight / nthreads + 1);
+      printf("CUDA kernel launch with [%d %d] blocks of [%d %d] threads\n",
+           blocksPerGrid.x, blocksPerGrid.y, threadsPerBlock.x,
+           threadsPerBlock.y);
       blurKernel_x<<<blocksPerGrid, threadsPerBlock>>>(
           deviceTempImageData, deviceInputImageData, imageWidth, imageHeight);
     }
      //vertical pass
     {
       dim3 blocksPerGrid(imageWidth / nthreads + 1);
+      printf("CUDA kernel launch with [%d %d] blocks of [%d %d] threads\n",
+           blocksPerGrid.x, blocksPerGrid.y, threadsPerBlock.x,
+           threadsPerBlock.y);
       blurKernel_y<<<blocksPerGrid, threadsPerBlock>>>(
           deviceOutputImageData, deviceTempImageData, imageWidth, imageHeight);
     }
